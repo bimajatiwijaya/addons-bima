4c67db80285d23312855d4c639a837fbe28c6f87
bima <bima@portcities.net>
2017-11-19 12:43:02 +0700

    [ADD] partner_approval only view and functional not done yet.

---
+++ partner_approval/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner approval',
+    'version': '1.0',
+    'category': 'module',
+    'author': 'Bima Wijaya',
+    'description': """
+    """,
+    'depends': ['data_approval'],
+    'data': [
+        'security/res_groups.xml',
+        'views/partner_approval_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
