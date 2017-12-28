f6637e98a99176be55f3e3df2b30c0a3fafe6207
Bima Wijaya <bimajatiwijaya@gmail.com>
2017-12-28 13:06:38 +0700

    [IMP] read data from machine and insert into odoo database.

--- solution_x302_s/models/hr_attendance.py
+++ solution_x302_s/models/hr_attendance.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+from datetime import datetime
 import requests
+import re
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class HrAttendance(models.Model):
@@ -8,6 +12,7 @@ class HrAttendance(models.Model):
 
     x302_s_user_id = fields.Integer('User ID in machine', help="Solution type X302-S User ID.")
 
+    @api.multi
     def sync_attendance(self):
         """
         0 -> pulang
@@ -19,11 +24,50 @@ class HrAttendance(models.Model):
         for emp in employees:
             
         """
-        r = requests.session()
-        datas = {'sdate': '2017-12-27','edate': '2017-12-27', 'uid': 3}
-        # datas = {}
-        response = r.post('http://172.16.5.11/form/Download',data=datas, stream=True)
-        block = response.text.split('\n')
-        print(block[0].split('\t')[0])
-        for elmt in range(len(block)):
-            print(elmt+1, block[elmt])
+        def get_data_from_x302_s(self, date, x302_user_id):
+            r = requests.session()
+            datas = {
+                'sdate': date, 'edate': date, 
+                'uid': x302_user_id}
+            block = []
+            try:
+                response = r.post('http://172.16.5.11/form/Download',data=datas, stream=True)
+                _logger.debug("XXXXXXXXXXXXXXXXXX")
+                _logger.debug(response.text)
+                block = response.text.split('\n')
+            except:
+                # Failed to connect machine 
+                pass
+            return block
+        
+        today = fields.date.today()
+        emp_obj = self.env['hr.employee']
+        attendance_obj = self.env['hr.attendance']
+        emp_ids = emp_obj.search([('x302_s_user_id', '!=', 0)])
+        today_attendance = attendance_obj.search([('check_in', '>=', str(today) + " 00:00:00")])
+        regex = re.compile(r'[\n\r\t]')
+        for emp_id in emp_ids:
+            emp_attendance = today_attendance.filtered(lambda x: x.employee_id.id == emp_id.id)
+            if not emp_attendance:
+                block = get_data_from_x302_s(self, date=today, x302_user_id=emp_id.x302_s_user_id)
+                if len(block) == 0:
+                    continue
+                block.pop()
+                last_idx = len(block) - 1
+                _logger.debug(str(block))
+                while(last_idx > -1):
+                    row = regex.sub(' ', block[last_idx])
+                    row = row.split(' ')
+                    _logger.debug(row)
+                    if row[5] == '0':
+                        _logger.debug("Create check in for "+ emp_id.name)
+                        respone = attendance_obj.create({
+                            'employee_id': emp_id.id,
+                            'check_in': row[2] + " " +row[3],
+                        })
+                        _logger.info("Succeed with ID : "+ str(respone.id))
+                    # How if user create manual and only fill check out for today ?
+                    last_idx -= 1
+            else:
+                _logger.debug(emp_attendance)
+                _logger.debug("UPDATE ATTENDANCE")
--- solution_x302_s/read_cloud.py
+++ solution_x302_s/read_cloud.py
@@ -1,10 +1,10 @@
 import requests
 
 r = requests.session()
-datas = {'sdate': '2017-12-27','edate': '2017-12-27', 'uid': 3}
+datas = {'sdate': '2017-12-28','edate': '2017-12-28', 'uid': 3}
 # datas = {}
 response = r.post('http://172.16.5.11/form/Download',data=datas, stream=True)
 block = response.text.split('\n')
-print(block[0].split('\t')[0])
+print(block[0].split('\t')[4])
 for elmt in range(len(block)):
-    print(elmt+1, block[elmt])
+    print(block[elmt])
